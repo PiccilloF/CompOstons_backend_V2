a87f31608608685b5e524f785268290fe4e9d9c8
PiccilloF <piccillofaustino@gmail.com>
2022-05-24 13:13:03 +0200

    initial commit, configuration and dependencies installation

---
+++ config/environment.js
@@ -0,0 +1,13 @@
+// Global environment variables file
+
+export default {
+  port: parseInt(process.env.PORT) || 8080,
+  nodeEnv: process.env.NODE_ENV || 'production',
+  saltRounds: parseInt(process.env.SALT_ROUNDS) || 10,
+  jwtAccessTokenSecret: 
+  process.env.JWT_ACCESS_TOKEN_SECRET || 
+  '7db9f8f494981d2311d38ff76b40cc6ac8d1a27951395e7f13dc92defa36e3d6',
+  jwtRefreshTokenSecret:
+  process.env.JWT_REFRESH_TOKEN_SECRET || 
+  'b4ee1ba4881d01beac71805953b1130c376c4dff7d3325a47e2a53a15d3b9714'
+}
